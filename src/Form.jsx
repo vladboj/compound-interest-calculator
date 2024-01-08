794a75a47e3ac389e694e16a3ec6bc3b2cc783c4
vladboj <vlad.bojan0@gmail.com>
2024-01-09 00:03:00 +0200

    add basic functionality

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
+
 function Form() {
+  const [resultToggled, setResultToggled] = useState(false);
+  const [finalAmount, setFinalAmount] = useState(null);
+
+  function computeFinalAmount(
+    initialInvestment,
+    interestRate,
+    timeLength,
+    compoundFrequency
+  ) {
+    const numberOfTimesCompoundedPerYear =
+      compoundFrequency === "anually" ? 1 : 12;
+    const finalAmount =
+      initialInvestment *
+      (1 + interestRate / numberOfTimesCompoundedPerYear) **
+        (numberOfTimesCompoundedPerYear * timeLength);
+    return Math.round(finalAmount * 100) / 100;
+  }
+
+  function handleCalculate() {
+    // Fetch input values and convert them to numbers
+    const inputs = document.querySelectorAll(".form-child > input");
+    const inputValues = [];
+    inputs.forEach((input) => inputValues.push(Number(input.value)));
+
+    // Compute final sum of money after x years of compounding
+    const initialInvestment = inputValues[0];
+    const interestRate = inputValues[1] / 100;
+    const timeLength = inputValues[2];
+
+    const compoundFrequency =
+      document.querySelector("#compoundFrequency").value;
+    const finalAmount = computeFinalAmount(
+      initialInvestment,
+      interestRate,
+      timeLength,
+      compoundFrequency
+    );
+
+    // Trigger rerendering with the new result
+    setResultToggled(true);
+
+    // Set the final amonunt using state
+    setFinalAmount(finalAmount);
+  }
+
   const formElements = [
     {
       id: "initialInvestment",
       label: "Initial Investment",
-    },
-    {
-      id: "monthlyContribution",
-      label: "Monthly Contribution",
     },
     {
       id: "interestRate",
@@ -19,15 +62,31 @@ function Form() {
   ];
 
   return (
-    <form>
-      {formElements.map((element) => (
+    <>
+      <form>
+        {formElements.map((element) => (
+          <div className="form-child" key={element.id}>
+            <label htmlFor={element.id}>{element.label}</label>
+            <input id={element.id} />
+          </div>
+        ))}
         <div className="form-child">
-          <label htmlFor={element.id}>{element.label}</label>
-          <input id={element.id} />
+          <label htmlFor="compoundFrequency">Compound Frequency</label>
+          <select id="compoundFrequency">
+            <option value="default">--Select Compound Frequency--</option>
+            <option value="anually">Anually</option>
+            <option value="monthly">Monthly</option>
+          </select>
         </div>
-      ))}
-      <input type="button" value="CALCULATE" />
-    </form>
+        <input type="button" value="CALCULATE" onClick={handleCalculate} />
+      </form>
+      {/* Conditionally render the result */}
+      {resultToggled && (
+        <div className="result">
+          <p>The final amount you will have is ${finalAmount}</p>
+        </div>
+      )}
+    </>
   );
 }
 
